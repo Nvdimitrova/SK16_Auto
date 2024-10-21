8ddd7ba99f2ca44987d606da3b1b23a7c94b6cfc
Nikol <nikolvd3@abv.bg>
2024-10-21 20:02:43 +0300

    IntelliJ

--- src/Exercises_04/BiggestNumber.java
+++ src/Exercises_04/BiggestNumber.java
@@ -4,13 +4,13 @@ public class BiggestNumber {
     public static void main(String[] args) {
         int[] list = {11, 22, 33, 444, 555, 66};
 
-        int largestNumber = list[0];
+        int biggestNumber = list[0];
 
         for (int i = 1; i < list.length; i++) {
-            if (list[i] > largestNumber) {
-                largestNumber = list[i];
+            if (list[i] > biggestNumber) {
+                biggestNumber = list[i];
             }
         }
-        System.out.println("Largest number is: " + largestNumber);
+        System.out.println("The biggest number is: " + biggestNumber);
     }
 }
